c9e48c029d2ac8a737c195ed12fdac2843f4cc78
Vladilav Yepifanov <evn1999@yandex.ru>
2019-02-20 15:50:28 +0300

    moved to package folder & build gradle updated

--- src/main/java/com/example/Serialization.java
+++ src/main/java/com/example/Serialization.java
@@ -1,6 +1,13 @@
 package com.example.serialize;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Serialization {
-    static void serialize(Object o, OutputStream outputStream);
-    static <T> T deserialize(InputStream, Class<T>);
+    static void serialize(Object o, OutputStream outputStream) {
+        
+    }
+    static <T> T deserialize(InputStream, Class<T>) {
+
+    }
 }
